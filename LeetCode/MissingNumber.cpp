8cd7dc07a17a4440d53d53d25ddea6b758de208c
Anupam19639749 <30anupam.agrawal@gmail.com>
2025-01-11 23:59:06 +0530

    Added a solution to find the missing number from an array

---
+++ LeetCode/MissingNumber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int result = 0;
+        int totalZOR = 0;
+        int arrayZOR = 0;
+
+        // XOR all numbers from 0 to n
+        for (int i = 0; i <= nums.size(); i++)
+        {
+            totalZOR ^= i;
+        }
+        // XOR all numbers in the array
+        for (int num : nums)
+        {
+            arrayZOR ^= num;
+        }
+
+        return totalZOR ^ arrayZOR;
+    }
+};
+int main()
+{
+    Solution sol;
+    vector<int> nums = {3, 0, 1};
+    cout << "The missing number is: " << sol.missingNumber(nums) << std::endl;
+    return 0;
+}
