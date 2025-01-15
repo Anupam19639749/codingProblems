918cfba1cd17b1d39f1aea38c4e4350a663cc04d
Anupam19639749 <30anupam.agrawal@gmail.com>
2025-01-16 00:11:33 +0530

    Added a solution to add two binary strings

---
+++ LeetCode/addBinaryString.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string addBinaryStrings(string a, string b)
+{
+    string result = "";                     // Store the binary result
+    int carry = 0;                          // Initialize carry
+    int i = a.size() - 1, j = b.size() - 1; // Start from the last characters of both strings
+
+    // Traverse both strings from the end
+    while (i >= 0 || j >= 0 || carry)
+    {
+        int bitA = (i >= 0) ? a[i--] - '0' : 0; // Get bit from a, or 0 if out of range
+        int bitB = (j >= 0) ? b[j--] - '0' : 0; // Get bit from b, or 0 if out of range
+
+        int sum = bitA + bitB + carry; // Calculate sum of current bits and carry
+        result += (sum % 2) + '0';     // Append the resulting bit (0 or 1) to result
+        carry = sum / 2;               // Update carry (0 or 1)
+    }
+
+    reverse(result.begin(), result.end()); // Reverse the result to get the correct order
+    return result;
+}
+
+int main()
+{
+    // Declaring string variables
+    string a;
+    string b;
+
+    // Take input/test case from the user
+    cout << "\n Enter the 1st and the 2nd binart=y string to add: ";
+    cin >> a >> b;
+
+    // Displaying the final sum of the binary strings which user has provided
+    cout << "The total of the binary strings " << a << " & " << b << " is: " << addBinaryStrings(a, b) << endl;
+    return 0;
+}
