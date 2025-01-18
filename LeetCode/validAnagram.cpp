57efe5a8a02bbe101d163f60fd53b09c38832f04
Anupam19639749 <30anupam.agrawal@gmail.com>
2025-01-19 00:29:06 +0530

    added a solution to check if two given strings are anagram of each other or not

---
+++ LeetCode/validAnagram.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isAnagram(string s, string t)
+{
+    if (s.length() != t.length())
+        return false;
+
+    vector<int> h(26);
+    for (int i = 0; i < s.length(); i++)
+    {
+        h[s[i] - 'a']++;
+    }
+
+    for (int i = 0; i < t.length(); i++)
+    {
+        int x = --h[t[i] - 'a'];
+        if (x < 0)
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    string s1 = "anagram";
+    string t1 = "nagaram";
+
+    string s2 = "egg";
+    string t2 = "add";
+
+    cout << "The given strings are anagram of each other: " << isAnagram(s1, t1) << endl;
+    cout << "The given strings are anagram of each other: " << isAnagram(s2, t2) << endl;
+    return 0;
+}
