15b9907de4b958946b49c94876e6d4263e1fc016
Anupam19639749 <30anupam.agrawal@gmail.com>
2025-02-02 23:49:57 +0530

    Added a solution to buld the pascal's triangle by taking the no. of rows user want

---
+++ LeetCode/PascalsTriangle.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> generate(int numRows)
+{
+    vector<vector<int>> triangle(numRows);
+
+    for (int i = 0; i < numRows; i++)
+    {
+        triangle[i].resize(i + 1, 1); // Resize and initialize with 1
+
+        for (int j = 1; j < i; j++)
+        {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+
+    return triangle;
+}
+
+int main()
+{
+    int numRows;
+    cout << "Enter number of rows: ";
+    cin >> numRows;
+
+    vector<vector<int>> result = generate(numRows);
+
+    for (const auto &row : result)
+    {
+        for (int num : row)
+        {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
